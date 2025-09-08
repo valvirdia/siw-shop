2c6f9c128ad3956f20b509ced16a819f3524ea91
valvirdia <val.virdia1@stud.uniroma3.it>
2025-09-08 17:41:36 +0200

    Primo push

---
+++ src/main/java/it/uniroma3/siw/siwshop/SiwShopApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiwShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/uniroma3/siw/siwshop/SiwShopApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
